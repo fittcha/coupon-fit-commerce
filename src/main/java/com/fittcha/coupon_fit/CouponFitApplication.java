1652cdbd2253659c6a65b36e56487446d007d249
cseungm <seung9232@gmail.com>
2025-12-11 21:40:20 +0900

    init: project init

---
+++ src/main/java/com/fittcha/coupon_fit/CouponFitApplication.java
@@ -0,0 +1,13 @@
+package com.fittcha.coupon_fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponFitApplication.class, args);
+	}
+
+}
